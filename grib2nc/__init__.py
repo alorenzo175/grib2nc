1fdb2e5a8e3a1fc0ba0d10d4723a3378d907c34f
Tony Lorenzo <alorenzo175@users.noreply.github.com>
2014-10-10 13:33:43 -0700

    updated fetch to get all the most recent forecasts not already downloaded

--- grib2nc/__init__.py
+++ grib2nc/__init__.py
@@ -1,1 +1,5 @@
 version = 0.1.0
+__all__ = ['grib2nc', 'fetch_forecasts']
+
+from grib2nc import Grib2NC
+from fetch_forecasts import HRRRFetcher
--- grib2nc/fetch_forecasts.py
+++ grib2nc/fetch_forecasts.py
@@ -13,6 +13,7 @@ import logging
 import traceback
 import datetime as dt
 import time
+import fcntl
 import argparse
 try:
     import ConfigParser as configparser
@@ -21,6 +22,7 @@ except ImportError:
 
 
 import pandas as pd
+import numpy as np
 try:
     import concurrent.futures as cf
 except ImportError:
@@ -238,7 +240,7 @@ class HRRRFetcher(object):
         if requeue:
             self.logger.warning('Must try to refetch some files')
             for filename, local_path in requeue:
-                self.logger.info('Trying to refetch filename')
+                self.logger.info('Trying to refetch %s' % filename)
                 try:
                     size = fetcher(filename, local_path)
                 except Exception as e:
@@ -256,16 +258,80 @@ class HRRRFetcher(object):
         self.logger.debug('Files retrieved are %s' % self.downloaded_files)
 
 
+def check_availability(config_path, levels):
+    """Check the HRRR availability website and compare
+    to already archived forecasts to fetch new forecasts
+    """
+    
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    avail_site = config.get('download_settings', 'availability_site')
+    avail_df = pd.io.html.read_html(avail_site, header=0, index_col=0, 
+                                    parse_dates=True, infer_types=False,
+                                    flavor='bs4')[0]
+
+    netcdf_folder = config.get('download_settings', 'netcdf_base_folder')
+    netcdf_filename = config.get('download_settings', 'netcdf_filename')
+
+    lsers = {}
+    for level in levels:
+        lsers[level] = pd.Series(np.zeros(len(avail_df.index)), 
+                                 index=avail_df.index)
+    
+    for atime in avail_df.index:
+        netcdf_path = netcdf_folder.format(year=atime.strftime('%Y'),
+                                           month=atime.strftime('%m'),
+                                           day=atime.strftime('%d'),
+                                           hour=atime.strftime('%H'))
+        if os.path.isdir(netcdf_path):
+            for level in levels:
+                ncfilename = netcdf_filename.format(level=level, 
+                    init_time=atime.strftime('%Y%m%d%H'))
+                if os.path.isfile(os.path.join(netcdf_path, ncfilename)):
+                    lsers[level].loc[atime] = 1
+                
+
+    avail_df = avail_df.join(pd.DataFrame(lsers))
+        
+    to_get = {}
+    for level in levels:
+        to_get[level] = sorted(avail_df[(avail_df['Status'] == 'Available') & 
+            (avail_df[level] == 0)].index.to_pydatetime())
+        
+    return to_get
+
+
+class Locker(object):
+    """Places a file lock so only one instance of the program
+    runs at a time
+    """
+    def __init__(self, lock_path='/tmp/fetch_forecasts.lock'):
+        self.lock_path = lock_path
+        try:
+            self.locked = open(self.lock_path, 'w+')
+            fcntl.lockf(self.locked, fcntl.LOCK_EX|fcntl.LOCK_NB)
+        except IOError:
+            logging.debug('File (%s) locked. Program probably running.' % 
+                          self.lock_path)
+            sys.exit(0)
+    def unlock(self):
+        fcntl.lockf(self.locked, fcntl.LOCK_UN)
+        os.remove(self.lock_path)
+
+
 def main():
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+
     argparser = argparse.ArgumentParser(
         description='Download HRRR grib files and store select fields in netCDF format')
+    
     argparser.add_argument('-v', '--verbose', help='Increase logging verbosity',
                            action='count')
     argparser.add_argument('-p', '--protocol', help='Download protocol',
                            choices=['ftp', 'http'], default='ftp')
-    argparser.add_argument('-c', '--config', help='Config file path')
-    argparser.add_argument('-l', '--level', default='subhourly',
+    argparser.add_argument('-c', '--config', help='Config file path',
+                           default='../settings.txt')
+    argparser.add_argument('-l', '--levels', default='subhourly',
                            help='HRRR level subtype; split multiple with commas')
     argparser.add_argument('-r', '--remove', action='store_true',
                            help='Remove grib files after conversion')
@@ -275,39 +341,60 @@ def main():
                            help="Don't convert the grib files to netCDF")
     argparser.add_argument('--no-overwrite', action='store_false',
                            help="Don't overwrite any files already downloaded")
-    argparser.add_argument('INITDT', 
-        help='Initilization datetime; split multiple with commas')
+    argparser.add_argument('--all', action='store_true',
+                           help="Get all the HRRR files not already archived")
+    argparser.add_argument('-i', '--init-times', 
+        help='Initilization datetimes; split multiple with commas')
     args = argparser.parse_args()
     
     if args.verbose == 1:
         logging.getLogger().setLevel(logging.INFO)
     elif args.verbose > 1:
         logging.getLogger().setLevel(logging.DEBUG)
-        
-    for atime in args.INITDT.split(','):
-        for alevel in args.level.split(','):
-            f = HRRRFetcher(atime, alevel, args.protocol, 
-                            args.config, args.threads)
-            f.fetch(overwrite=args.no_overwrite)
 
-            if not args.no_convert:
-                from grib2nc import Grib2NC
-                g2nc = Grib2NC(f.init_time, f.level, f.config_path)
-                g2nc.load_into_netcdf()
+    global lock
+    lock = Locker()
 
-            if args.remove and not args.no_convert:
-                for apath in f.downloaded_files:
-                    os.remove(apath)
-                    if not os.listdir(os.path.dirname(apath)):
-                        os.rmdir(os.path.dirname(apath))
-            
+    def loop(atime, alevel, args):
+        f = HRRRFetcher(atime, alevel, args.protocol, 
+                        args.config, args.threads)
+        f.fetch(overwrite=args.no_overwrite)
+
+        if not args.no_convert:
+            from grib2nc import Grib2NC
+            g2nc = Grib2NC(f.init_time, f.level, f.config_path)
+            g2nc.load_into_netcdf()
+            del g2nc
+
+        if args.remove and not args.no_convert:
+            for apath in f.downloaded_files:
+                os.remove(apath)
+                if not os.listdir(os.path.dirname(apath)):
+                    os.rmdir(os.path.dirname(apath))
+
+
+    if args.all:
+        files_to_get = check_availability(args.config, args.levels.split(','))
+        for alevel, thetimes in files_to_get.items():
+            for atime in thetimes:
+                loop(atime, alevel, args)
+    elif args.init_times is not None:
+        for atime in args.init_times.split(','):
+            for alevel in args.levels.split(','):
+                loop(atime,alevel, args)
+    else:
+        argparser.error('Either --all or -i/--init-times must be specified')
+    lock.unlock()
+
 
 def exceptionlogging(*exc_info):
     text = "".join(traceback.format_exception(*exc_info))
     logger = logging.getLogger()
     logger.exception("Unhandled exception: %s" % text)
+    lock.unlock()
 
 
 if __name__ == '__main__':
+    sys.settrace
     sys.excepthook = exceptionlogging
     main()
