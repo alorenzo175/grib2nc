a9e50ee515cad14fb24d7b7768a398ee598419f0
Tony Lorenzo <alorenzo175@users.noreply.github.com>
2015-02-04 15:03:54 -0700

    fixed import

--- grib2nc/__init__.py
+++ grib2nc/__init__.py
@@ -2,4 +2,4 @@ version = '0.2.0'
 __all__ = ['grib2nc', 'fetch_forecasts']
 
 from .grib2nc import Grib2NC, EmptyError
-from fetch_forecasts import HRRRFetcher
+from .fetch_forecasts import HRRRFetcher
