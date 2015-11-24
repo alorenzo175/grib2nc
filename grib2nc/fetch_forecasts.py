d1861f81b1181a5cb7d2fc78ec0b0bac43045ac9
Tony Lorenzo <alorenzo175@users.noreply.github.com>
2015-11-23 23:15:03 -0700

    fix umask

--- grib2nc/fetch_forecasts.py
+++ grib2nc/fetch_forecasts.py
@@ -87,7 +87,7 @@ class HRRRFetcher(object):
             level=level)
 
         if not os.path.isdir(self.base_path):
-            os.makedirs(self.base_path, mode=0o2775)
+            os.makedirs(self.base_path, mode=0o2775>)
         self.downloaded_files = []
 
         self.nthreads = threads or self.config.getint(
@@ -393,6 +393,8 @@ def main():
     argparser.add_argument('-i', '--init-times',
         help='Initilization datetimes; split multiple with commas')
     args = argparser.parse_args()
+
+    os.umask(0)
 
     if args.verbose == 1:
         logging.getLogger().setLevel(logging.INFO)
